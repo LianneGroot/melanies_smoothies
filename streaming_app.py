bb9f86aa5d88736dd34a5d1b94a559d32ab89253
Lianne Groot <167186646+LianneGroot@users.noreply.github.com>
2025-03-12 13:16:34 +0100

    Update streaming_app.py

--- streaming_app.py
+++ streaming_app.py
@@ -55,5 +55,5 @@ if ingredients_list:
         
         st.subheader(fruit_chosen + ' Nutrition Information')
         import requests
-        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + fruit_chosen)
+        smoothiefroot_response = requests.get("https://my.smoothiefroot.com/api/fruit/" + search_on)
         sf_df = st.dataframe(data=smoothiefroot_response.json(), use_container_width=True)
